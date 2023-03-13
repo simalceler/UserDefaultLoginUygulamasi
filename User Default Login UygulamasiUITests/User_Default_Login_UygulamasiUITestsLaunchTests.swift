8845010f7042718aa385a7dc3eb820be96ede959
simalceler <celersimal@gmail.com>
2023-03-13 13:17:00 +0300

    Initial Commit

---
+++ User Default Login UygulamasiUITests/User_Default_Login_UygulamasiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  User_Default_Login_UygulamasiUITestsLaunchTests.swift
+//  User Default Login UygulamasiUITests
+//
+//  Created by Şimal Çeler on 13.03.2023.
+//
+
+import XCTest
+
+final class User_Default_Login_UygulamasiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
