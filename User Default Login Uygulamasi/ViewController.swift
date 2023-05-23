60019b84fe6c3b77d080fe98a2bc703dda7d12a1
simalceler <celersimal@gmail.com>
2023-05-23 12:17:33 +0300

    git add.

--- User Default Login Uygulamasi/ViewController.swift
+++ User Default Login Uygulamasi/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textfieldkullaniciAdi: UITextField!
+    
+    @IBOutlet weak var textfieldsifre: UITextField!
+    
+    let d = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let ka = d.string(forKey: "kullaniciAdi") ?? "bos"
+        let s = d.string(forKey: "sifre") ?? "bos"
+        
+        if ka != "bos" && s != "bos" {
+            performSegue(withIdentifier: "girisToanasayfa", sender: nil)
+        }
+        
     }
 
-
+    @IBAction func girisYap(_ sender: Any) {
+        
+        if let ka = textfieldkullaniciAdi.text , let s = textfieldsifre.text {
+            
+            if ka == "admin" && s == "123456" {
+                
+                d.set(ka, forKey: "kullaniciAdi")
+                d.set(s, forKey: "sifre")
+                
+                performSegue(withIdentifier: "girisToanasayfa", sender: nil)
+                
+            }else{
+                print("Hatalı Giriş")
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+    }
 }
 
